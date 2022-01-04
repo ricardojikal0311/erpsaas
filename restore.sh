8a0da5ac64d012205f2647c608df2e9c2f73af2f
ricardojikal0311 <15992876+ricardojikal0311@users.noreply.github.com>
2022-01-04 11:21:42 +0000

    Revert Development 3.x

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2021-01-01"  # YYYY-MM-DD
-END_DATE="2021-03-28"    # YYYY-MM-DD
+START_DATE="2022-01-01"  # YYYY-MM-DD
+END_DATE="2022-03-28"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=1
